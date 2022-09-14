4c36a217cbfa70ed2fe10f75e146bf3c755a05a6
lemarzala <lemarzala.tarakhil123@gmail.com>
2022-09-14 11:38:08 -0600

    push

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// #!/user/bin/env node
+#!/usr/bin/env node
 
-console.log("Running test!!....");
+const Runner = require("./runner");
+const runner = new Runner();
+
+const run = async () => {
+  await runner.collectFiles(process.cwd());
+  runner.runTests();
+};
+
+run();
